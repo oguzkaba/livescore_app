73601bec64bcfeee80b83e7bd5419c6cd905df40
Oguz KABA <oguz-kaba@hotmail.com>
2022-06-23 16:53:04 +0300

    cmt-2

--- lib/app/constants/constants.dart
+++ lib/app/constants/constants.dart
@@ -11,15 +11,36 @@ const Color darkGrey = Color(0xff555555);
 const Color focusPink = Color(0xffFF5757);
 const Color midGrey = Color(0xff555555);
 const Color lightGrey = Color(0xff777777);
+
 const Color transparent = Colors.transparent;
 const Color white = Colors.white;
 
 const String logo = "assets/svg/live_logo.svg";
 
+const List<Color> cardColor = [
+  Color(0xff2648D1),
+  Color(0xffEAEAEA),
+  Color(0xffFB5266)
+];
+
+const List<Color> circleColor = [
+  Color(0xff57574B),
+  Color(0xffEAEAEA),
+  Color(0xffFB5266),
+  Color(0xff39003C),
+  Color(0xffD5DC3D),
+];
+
+const List<String> newsImageList = [
+  "https://ichef.bbci.co.uk/images/ic/896x504/p08tldgz.jpg",
+  "https://ichef.bbci.co.uk/live-experience/cps/480/cpsprodpb/260B/production/_125593790_jamie_overton_reuters2.jpg",
+  "https://images.theconversation.com/files/466578/original/file-20220601-49499-zn0ef9.jpg?ixlib=rb-1.1.0&rect=23%2C0%2C2627%2C1313&q=45&auto=format&w=668&h=350&fit=crop"
+];
+
 const Map<String, IconData> bottomItemList = {
   "Home": FontAwesomeIcons.house,
   "Football": FontAwesomeIcons.futbol,
-  "Standings": FontAwesomeIcons.futbol,
+  "Standings": FontAwesomeIcons.chartColumn,
   "Statistic": FontAwesomeIcons.chartBar,
   "Profile": FontAwesomeIcons.user
 };
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:livescore_app/app/constants/constants.dart';
 import '../controllers/home_controller.dart';
@@ -9,76 +8,179 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final HomeController homeController = Get.put(HomeController());
-    return Scaffold(
-        appBar: AppBar(
-          iconTheme: IconThemeData(color: lightGrey),
-          shadowColor: transparent,
-          backgroundColor: transparent,
-          title: SvgPicture.asset(
-            logo,
-            height: 30,
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 80,
+            iconTheme: IconThemeData(color: lightGrey),
+            shadowColor: transparent,
+            backgroundColor: transparent,
+            title: SvgPicture.asset(
+              logo,
+              height: 30,
+            ),
+            centerTitle: true,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8),
+                child: IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+              )
+            ],
           ),
-          centerTitle: true,
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
-        ),
-        drawer: Drawer(),
-        body: Column(children: [
+          drawer: Drawer(),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 18.0),
+              child: Column(children: [
+                _topCategoryListView(),
+                _livescoreListView(),
+                _newsListView()
+              ]),
+            ),
+          ),
+          bottomNavigationBar: Obx(
+            () => _buildBottomNav(homeController),
+          )),
+    );
+  }
+
+  Widget _newsListView() {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
           Align(
-              alignment: Alignment.topRight,
-              child: Container(
-                margin: EdgeInsets.only(top: 20),
+            alignment: Alignment.topLeft,
+            child: Text("Latest News",
+                style: TextStyle(
+                    fontSize: 20, color: white, fontWeight: FontWeight.bold)),
+          ),
+          Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            for (var i = 0; i < 3; i++)
+              Container(
+                margin: const EdgeInsets.only(top: 18.0),
+                //padding: const EdgeInsets.only(top: 18.0),
                 width: Get.width * .9,
-                height: Get.height * .15,
+                height: Get.height * .25,
                 decoration: BoxDecoration(
-                    color: dark,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        bottomLeft: Radius.circular(15))),
-                child: Column(
+                    image:
+                        DecorationImage(image: NetworkImage(newsImageList[i])),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+              )
+          ]),
+        ]);
+  }
+
+  Widget _livescoreListView() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 25, 18, 0),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text("Live Matches",
+                style: TextStyle(
+                    fontSize: 20, color: white, fontWeight: FontWeight.bold)),
+            Text("VIEW ALL",
+                style: TextStyle(color: lightGrey, fontWeight: FontWeight.bold))
+          ]),
+        ),
+        Container(
+          width: Get.width,
+          height: Get.height * .3,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemCount: 3,
+              itemBuilder: ((context, index) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Padding(
-                      padding: const EdgeInsets.fromLTRB(18, 18, 18, 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Leagues",
-                              style: TextStyle(fontSize: 18, color: white)),
-                          Text("VIEW ALL",
-                              style: TextStyle(
-                                  color: midGrey, fontWeight: FontWeight.bold)),
-                        ],
+                      padding: const EdgeInsets.only(right: 18.0),
+                      child: Container(
+                        //margin: EdgeInsets.fromLTRB(8, 20, 8, 20),
+                        width: Get.width * .37,
+                        height: Get.height * .23,
+                        //child: ,
+                        decoration: BoxDecoration(
+                            color: cardColor[index],
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
                       ),
                     ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 18.0),
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          shrinkWrap: true,
-                          itemBuilder: ((context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.all(5),
-                              child: CircleAvatar(radius: 35),
-                            );
-                          }),
-                          itemCount: 20,
-                        ),
-                      ),
-                    )
                   ],
-                ),
-              )),
-        ]),
-        bottomNavigationBar: Obx(
-          () => _buildBottomNav(homeController),
-        ));
+                );
+              })),
+        ),
+      ],
+    );
+  }
+
+  Widget _topCategoryListView() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              margin: EdgeInsets.only(top: 20),
+              width: Get.width * .9,
+              height: Get.height * .2,
+              decoration: BoxDecoration(
+                  color: dark,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      bottomLeft: Radius.circular(25))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(18, 18, 18, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Leagues",
+                            style: TextStyle(fontSize: 18, color: white)),
+                        Text("VIEW ALL",
+                            style: TextStyle(
+                                color: midGrey, fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  ),
+                  Flexible(
+                    fit: FlexFit.loose,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 18.0),
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemBuilder: ((context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(5),
+                            child: CircleAvatar(
+                                radius: 30,
+                                backgroundColor: circleColor[index]),
+                          );
+                        }),
+                        itemCount: circleColor.length,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )),
+      ],
+    );
   }
 
   BottomNavigationBar _buildBottomNav(HomeController homeController) {
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
       selectedItemColor: focusPink,
+      selectedIconTheme: IconThemeData(size: 26),
       unselectedItemColor: midGrey,
       iconSize: 20,
       showSelectedLabels: false,
